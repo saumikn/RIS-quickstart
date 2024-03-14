ca64647788a61daa5baead1bb8c293c50118601b
Saumik Narayanan <saumik.narayanan@outlook.com>
2024-03-14 19:43:47 +0000

    initial version of environment.yml and 01 and 02 scripts

--- jupyter-cpu.sh
+++ jupyter-cpu.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/bash
 
-port="8228"
-cores="8"
-memory="100GB"
-tag="maialib"
+# Change this to your own advisor's faculty ID, used for storage. This is not a parameter
+facultyId="chien-ju.ho" 
+
+# Change this to your own Docker Repo ID. You can also edit with the -d tag
+docker="saumikn/quickstart:latest" 
+
+# How many hours your job runs for. You can edit this with the -t tag. Max of 672 hour for any job
+timeLimit="168"
+
+# If you're launching multiple Jupyter Lab instances, use the -p parameter to change the port for each one, so they don't collide.
+# Especially if your environments crash with the error "Bind for 0.0.0.0:8401 failed: port is already allocated"
+port="8401"
+
+# If you need multiple CPUs, use the -c parameter to add more CPUs
+cores="1" 
+
+# If you need more RAM, use the -m parameter to use more RAM
+memory="16GB" 
+
 while [ $# -gt 0 ]; do
   case "$1" in
     -p)
@@ -15,11 +30,11 @@ while [ $# -gt 0 ]; do
     -m)
       memory="$2"
       ;;
-    -t)
-      tag="$2"
+    -d)
+      docker="$2"
       ;;
-    -x)
-      exclusive="$2"
+    -t)
+      timeLimit="$2"
       ;;
     *)
       printf "***************************\n"
@@ -31,6 +46,4 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-echo "$select$port"
-
-LSF_DOCKER_VOLUMES="/scratch1/fs1/chien-ju.ho:/scratch1/fs1/chien-ju.ho /storage1/fs1/chien-ju.ho/Active:/storage1/fs1/chien-ju.ho/Active" LSF_DOCKER_PORTS="$port:$port" PATH="/opt/conda/bin:/usr/local/cuda/bin:$PATH" bsub -q general -n $cores -M $memory -R "select[port$port=1] span[hosts=1] rusage[mem=$memory]" -G compute-chien-ju.ho -J jupyter-cpu -a "docker(saumikn/chesstrainer:$tag)" jupyter lab --allow-root --ip=0.0.0.0 --port $port --no-browser
+LSF_DOCKER_VOLUMES="/scratch1/fs1/$facultyId:/scratch1/fs1/$facultyId /storage1/fs1/$facultyId/Active:/storage1/fs1/$facultyId/Active" LSF_DOCKER_PORTS="$port:$port" PATH="/opt/conda/bin:/usr/local/cuda/bin:$PATH" bsub -q general -n $cores -M $memory -R "select[port$port=1] span[hosts=1] rusage[mem=$memory]" -G compute-$facultyId -J "jupyter-cpu-$docker" -a "docker($docker)" -w ${timeLimit}:00 jupyter lab --allow-root --ip=0.0.0.0 --port $port --no-browser
--- jupyter-gpu.sh
+++ jupyter-gpu.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/bash
 
-port="8228"
-cores="8"
-memory="100GB"
-gpus="1"
-model="TeslaV100_SXM2_32GB"
-# model="NVIDIAA40"
-docker="saumikn/chesstrainer"
-tag="empty"
-exclusive="no"
-select="gpuhost,"
+# Change this to your own advisor's faculty ID, used for storage. This is not a parameter
+facultyId="chien-ju.ho" 
+
+# Change this to your own Docker Repo ID. You can also edit with the -d tag
+docker="saumikn/quickstart:latest" 
+
+# How many hours your job runs for. You can edit this with the -t tag. Max of 672 hour for any job
+timeLimit="168"
+
+# If you're launching multiple Jupyter Lab instances, use the -p parameter to change the port for each one, so they don't collide.
+# Especially if your environments crash with the error "Bind for 0.0.0.0:8401 failed: port is already allocated"
+port="8401"
+
+# If you need multiple CPUs, use the -c parameter to add more CPUs
+cores="1" 
+
+# If you need more RAM, use the -m parameter to use more RAM
+memory="16GB" 
+
+# If you need multiple GPUs, use the -g parameter. Unless you're really smart with your coding, multiple GPUs is almost never useful
+gpus="1" 
+
+# Other option is "NVIDIAA40", but I haven't gotten this to work
+model="TeslaV100_SXM2_32GB" 
+
+# If you need exclusive access to a GPU, or you're willing to share. Sharing is better practice as part of the RIS community,
+# but there may be times when you need exclusive access. You can edit this with the -x parameter
+exclusive="no" 
+
+
 while [ $# -gt 0 ]; do
   case "$1" in
     -p)
@@ -23,13 +43,12 @@ while [ $# -gt 0 ]; do
       ;;
     -g)
       gpus="$2"
-      select="gpuhost,"
       ;;
     -d)
       docker="$2"
       ;;
     -t)
-      tag="$2"
+      timeLimit="$2"
       ;;
     -x)
       exclusive="$2"
@@ -44,6 +63,4 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-echo "$select$port"
-
-LSF_DOCKER_VOLUMES="/scratch1/fs1/chien-ju.ho:/scratch1/fs1/chien-ju.ho /storage1/fs1/chien-ju.ho/Active:/storage1/fs1/chien-ju.ho/Active" LSF_DOCKER_PORTS="$port:$port" PATH="/opt/conda/bin:/usr/local/cuda/bin:$PATH" bsub -q general -n $cores -M $memory -R "select[${select}port$port=1] span[hosts=1] rusage[mem=$memory]" -G compute-chien-ju.ho -J "jupyter-gpu-$tag" -gpu "num=$gpus:gmodel=$model:j_exclusive=$exclusive" -a "docker($docker:$tag)" jupyter lab --allow-root --ip=0.0.0.0 --port $port --no-browser
+LSF_DOCKER_VOLUMES="/scratch1/fs1/$facultyId:/scratch1/fs1/$facultyId /storage1/fs1/$facultyId/Active:/storage1/fs1/$facultyId/Active" LSF_DOCKER_PORTS="$port:$port" PATH="/opt/conda/bin:/usr/local/cuda/bin:$PATH" bsub -q general -n $cores -M $memory -R "select[gpuhost,port$port=1] span[hosts=1] rusage[mem=$memory]" -G compute-$facultyId -J "jupyter-gpu-$tag" -gpu "num=$gpus:gmodel=$model:j_exclusive=$exclusive" -a "docker($docker)" -w ${timeLimit}:00 jupyter lab --allow-root --ip=0.0.0.0 --port $port --no-browser
